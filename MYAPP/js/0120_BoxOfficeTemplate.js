398f9672b8ee5fc444dad3bf607417e6ba8248e3
Newbi7 <anelitgel12@gmail.com>
2023-01-20 17:28:07 +0900

    no message

---
+++ MYAPP/js/0120_BoxOfficeTemplate.js
@@ -0,0 +1,34 @@
+
+function myFunc() {
+
+    $.ajax({
+        url: 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json',
+        async: true,
+        data: {
+            key: '756e6c8584bda7b0084ecb882f94a26a', targetDt: '20230119'},
+            type : 'GET',
+            dataType: 'json',
+        success: function(data){
+            let rank = $('#rank').text(data.boxOfficeResult.dailyBoxOfficeList[0].rank);
+            let title = $('#title').text(data.boxOfficeResult.dailyBoxOfficeList[0].movieNm);
+            let salesAcc = $('#salesAcc').text(data.boxOfficeResult.dailyBoxOfficeList[0].salesAcc);
+            let openDt = $('#openDt').text(data.boxOfficeResult.dailyBoxOfficeList[0].openDt);
+
+            let tr = $("<tr></tr>");
+            let rk = $("<td></td>").text(rank);
+            let tt = $("<td></td>").text(title);
+            let sa = $("<td></td>").text(salesAcc);
+            let od = $("<td></td>").text(openDt);
+
+            tr.append(rank);
+            tr.append(title);
+            tr.append(salesAcc);
+            tr.append(openDt);
+
+            $('tbody').append(tr);
+        },
+        error: function() {
+            console.log('뭔가이상');
+        }   
+        });
+    };
---
+++ 잡다한 코드/memo.md
@@ -0,0 +1,14 @@
+GET방식 -> Query String이용해서 URL뒤에 붙여요
+
+key = 756e6c8584bda7b0084ecb882f94a26a
+
+targetDt = 20230119
+
+Rest key = 5559977ec58da2ce68a5aba6c2027c04
+
+
+http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json
+
+
+
+Query String => ?key=756e6c8584bda7b0084ecb882f94a26a&targetDt=20230119
