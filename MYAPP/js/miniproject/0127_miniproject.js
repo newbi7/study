aa1c0f02236a53cde1d3ae581cdf0ab87e99940a
Newbi7 <anelitgel12@gmail.com>
2023-01-31 17:44:06 +0900

    no message

--- MYAPP/js/miniproject/0127_miniproject.js
+++ MYAPP/js/miniproject/0127_miniproject.js
@@ -15,11 +15,11 @@ Vue.component('globalpage-component-a', {
         </thead>
         <tbody>
             <tr v-for="(item, idx) in movieArr">
-                <td><input type="checkbox" v-model="checkedValues[idx]" v-on:click="checkBoxNum"></td>
+                <td><input type="checkbox" v-model="checkedValues[item.rnum - 1]"></td>
                 <td>{{item.rank}}</td>
                 <td><img v-bind:src="item.imgurl"></td>
                 <td>
-                    <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal" v-on:click="detailSearch">
+                    <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal" v-on:click="detailSearch(item.movieCd)">
                     {{item.movieNm}}
                     </a>
                 </td>
@@ -34,20 +34,20 @@ Vue.component('globalpage-component-a', {
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-scrollable">
             <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">영화 상세정보, movieNm</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                openDt 제작년도
-                nations nation nationNm 제작국가
-                directors director peopleNm 감독
-                actors actor peopleNm 배우
-                showTm 상영시간
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            </div>
+                  <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">{{movieDetail.movieNm}}</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                      <tr>제작년도   {{movieDetail.prdtYear}}</tr>
+                      <tr>제작국가   <span v-for="nations in movieDetail.nations">{{nations.nationNm}}</span></tr>
+                      <tr>감독 <span v-for="director in movieDetail.directors">{{director.peopleNm}}</span></tr>
+                      <tr>배우 <span v-for="actor in movieDetail.actors">{{actor.peopleNm}}</span></tr>
+                      <tr>상영시간 {{movieDetail.showTm}}분</tr>
+                  </div>
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                 </div>
             </div>
         </div>
         </div>
@@ -58,37 +58,38 @@ Vue.component('globalpage-component-a', {
     },
     data() {
         return {
-            checkedValues: []
+            movieDetail: {}
         }
     },
-
     props: {
-        movieArr: Array
+        movieArr: Array,
+        movieCd: String,
+        checkedValues: Array
+    },
+    model: {
+        prop: 'checkedValues',
+        event: 'updatedChecked'
     },
     methods: {
         deleteClick: function (idx) {
             this.$delete(this.movieArr, idx);
+
         },
 
-        checkBoxNum: function () {
-            this.$emit("check", this.checkedValues)
-        },
-
-        detailSearch: function () {
+        detailSearch: function (movieCd) {
 
             axios.get('http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json', {
 
                 params: {
                     key: '756e6c8584bda7b0084ecb882f94a26a',
-                    movieCd: '20112207'
+                    movieCd: movieCd
                 },
 
                 responseType: 'json'
 
             }).then((movieDetail) => {
                 console.log('코비스세부 접속성공');
-                this.movieDetailArr = movieDetail.data.movieInfoResult.movieInfo;
-                console.log(this.movieDetailArr);
+                this.movieDetail = movieDetail.data.movieInfoResult.movieInfo;
 
             }).catch(function (error) {
                 console.log('코비스세부가 이상해요');
@@ -103,7 +104,7 @@ new Vue({
     data: {
         searchDate: '',
         movieArr: [],
-        checked: [],
+        checkedValues: []
     },
 
     methods: {
@@ -113,15 +114,17 @@ new Vue({
 
         },
 
-        deleteCheckClick: function (checked) {
-            // if (this.checkedValues = false) {
-            //     checknum=1;
-            // }
-            // console.log(checknum);
-            console.log(this.checkedValues);
-            this.$delete(this.movieArr, this.checkedvalues = true);
+        deleteCheckClick: function () {
 
-            console.log('삭제');
+            this.checkedValues.forEach((el, idx) => {
+
+                if(el) {
+                    this.movieArr = this.movieArr.filter(movie => Number.parseInt(movie.rnum) - 1 !== idx);
+                }
+
+                });
+                this.checkedValues = [];
+
         },
 
         dateSearch: function () {
@@ -139,7 +142,6 @@ new Vue({
             }).then((movieData) => {
                 console.log('코비스 접속성공');
                 this.movieArr = movieData.data.boxOfficeResult.dailyBoxOfficeList;
-
                 this.movieArr.forEach((el) => {
                     axios.get('https://dapi.kakao.com/v2/search/image', {
 
